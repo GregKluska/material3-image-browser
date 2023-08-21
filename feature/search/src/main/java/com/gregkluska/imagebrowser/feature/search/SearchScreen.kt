e9d1f7b5c23347ed91ccbd8735d913db967f3fdc
Greg Kluska <kluskagreg@gmail.com>
2023-08-21 17:42:05 +0100

    Add common ui module and add initial screen to the search

---
+++ feature/search/src/main/java/com/gregkluska/imagebrowser/feature/search/SearchScreen.kt
@@ -0,0 +1,15 @@
+package com.gregkluska.imagebrowser.feature.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchScreen() {
+
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+
+}
---
+++ feature/search/src/main/java/com/gregkluska/imagebrowser/feature/search/SearchViewModel.kt
@@ -0,0 +1,9 @@
+package com.gregkluska.imagebrowser.feature.search
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SearchViewModel: ViewModel() {
+
+}

ed5f532fab12e2f3248ce06cd4b58b645afbc6e2
Greg Kluska <kluskagreg@gmail.com>
2023-08-23 14:11:25 +0100

    Add scrollable to detail screen

--- feature/detail/src/main/java/com/gregkluska/imagebrowser/feature/detail/DetailScreen.kt
+++ feature/detail/src/main/java/com/gregkluska/imagebrowser/feature/detail/DetailScreen.kt
@@ -1,7 +1,20 @@
 package com.gregkluska.imagebrowser.feature.detail
 
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import com.gregkluska.imagebrowser.common.ui.ImageContainer
 import com.gregkluska.imagebrowser.core.model.Image
 
@@ -10,11 +23,19 @@ fun DetailScreen(
     image: Image?,
     onEvent: (DetailEvent) -> Unit,
 ) {
-    println(image)
-    ImageContainer(
-        url = image?.url ?: "",
-        alt = image?.description ?: "",
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
     ) {
-        Text("Author: ${image?.author?.name?:"Loading"}")
+        ImageContainer(
+            modifier = Modifier.padding(16.dp),
+            url = image?.url ?: "",
+            alt = image?.description ?: "",
+        ) {
+            Text("Author: ${image?.author?.name ?: "Loading"}")
+        }
     }
+
 }
